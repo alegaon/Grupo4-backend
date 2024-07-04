baea189030d8cb343abcc23c8a896cd61f2af66d
Alvaro Cubiella <alvarocubiella@gmail.com>
2024-07-04 03:03:33 -0300

    Carga imagenes /statics

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 from depesca.views import *
 from depesca.database import *
+import depesca
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+# Obtener el directorio del módulo depesca
+depesca_dir = os.path.dirname(depesca.__file__)
+
+# Crear el directorio static dentro del directorio depesca si no existe
+static_dir = os.path.join(depesca_dir, 'static')
+app.static_url_path = static_dir
+if not os.path.exists(static_dir):
+    os.makedirs(static_dir)
 
 # Rutas
 app.route('/', methods=['GET'])(index)
@@ -14,14 +25,18 @@ app.route('/api/articulos/activos', methods=['GET'])(get_active_articles)
 
 app.route('/api/articulo/create', methods=['POST'])(new_article)
 
-app.route('/api/articulos/update/<int:id_article>', methods=['PUT'])(update_article)
-app.route('/api/articulos/activo/<int:id_article>', methods=['DELETE'])(active_article)
+app.route('/api/articulos/update/<int:id_article>',
+          methods=['PUT'])(update_article)
+app.route('/api/articulos/activo/<int:id_article>',
+          methods=['DELETE'])(active_article)
 
 # Realizo una prueba de conexion a la base de datos
 test_conn()
 create_articulos()
 
 init_app(app)
+# permitir solicitudes desde cualquier origen
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- depesca/views.py
+++ depesca/views.py
@@ -1,6 +1,9 @@
-from flask import jsonify, request
+from flask import current_app, jsonify, request
+from werkzeug.utils import secure_filename
 from datetime import datetime
 from depesca.model import Articulos
+
+import os
 
 
 def index():
@@ -32,12 +35,33 @@ def get_active_articles():
     return jsonify([articulo.serializer() for articulo in articulos])
 
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+def __allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 def new_article():
     # Estaria bueno implementar algo que compruebe que las keys del diccionario estan todas para
     # que no se rompa.
     data = request.json
+    # Assuming 'img_portada' contains the file path
+    img_portada = data.get('img_portada')
+
+    # Handle file upload
+    if img_portada:
+        # Assuming img_portada contains the path to the image file on the server
+        filename = os.path.basename(img_portada)
+        dest = os.path.join(current_app.static_url_path, filename)
+
+        # Move the uploaded file to the static directory
+        os.rename(img_portada, dest)
+        # Store relative path in database
+        img_portada = os.path.join('\static', filename)
+
     new_art = Articulos(
-        img_portada=data['img_portada'],
+        img_portada=img_portada,
         titulo=data['titulo'],
         descripcion=data['descripcion'],
         comida=data['comida'],
@@ -76,49 +100,23 @@ def update_article(id_article):
     if not articulo:
         return jsonify({'message': 'Article not found'}), 404
     data = request.json
+    # Si se envia un diccionario vacio, se retorna un mesaje.
     if not data:
-        return jsonify({'message': 'Nada que actualizar'}), 404
-    # los if son para corroborar que la Key del diccionario a actualizar fue enviada, sino la pasa por alto. De lo contrario se rompe el Update
-    if 'img_portada' in data:
-        articulo.img_portada = data['img_portada']
-    if 'titulo' in data:
-        articulo.titulo = data['titulo']
-    if 'descripcion' in data:
-        articulo.descripcion = data['descripcion']
-    if 'comida' in data:
-        articulo.comida = data['comida']
-    if 'embarcacion' in data:
-        articulo.embarcacion = data['embarcacion']
-    if 'guia' in data:
-        articulo.guia = data['guia']
-    if 'equipos' in data:
-        articulo.equipos = data['equipos']
-    if 'carnada' in data:
-        articulo.carnada = data['carnada']
-    if 'wifi' in data:
-        articulo.wifi = data['wifi']
-    if 'hospedaje' in data:
-        articulo.hospedaje = data['hospedaje']
-    if 'atencion' in data:
-        articulo.atencion = data['atencion']
-    if 'salvavidas' in data:
-        articulo.salvavidas = data['salvavidas']
-    if 'info_descripcion' in data:
-        articulo.info_descripcion = data['info_descripcion']
-    if 'info_ubicacion' in data:
-        articulo.info_ubicacion = data['info_ubicacion']
-    if 'info_img_principal' in data:
-        articulo.info_img_principal = data['info_img_principal']
-    if 'info_img_descripcion' in data:
-        articulo.info_img_descripcion = data['info_img_descripcion']
-    if 'inf_img_1' in data:
-        articulo.inf_img_1 = data['inf_img_1']
-    if 'inf_img_2' in data:
-        articulo.inf_img_2 = data['inf_img_2']
-    if 'inf_img_3' in data:
-        articulo.inf_img_3 = data['inf_img_3']
-    if 'inf_img_4' in data:
-        articulo.inf_img_4 = data['inf_img_4']
+        return jsonify({'message': 'Nothing to update.', 'id': id_article}), 404
+
+    # Lista de los campos que se pueden actualizar
+    campos_actualizables = [
+        'img_portada', 'titulo', 'descripcion', 'comida', 'embarcacion', 'guia',
+        'equipos', 'carnada', 'wifi', 'hospedaje', 'atencion', 'salvavidas',
+        'info_descripcion', 'info_ubicacion', 'info_img_principal',
+        'info_img_descripcion', 'inf_img_1', 'inf_img_2', 'inf_img_3', 'inf_img_4'
+    ]
+
+    # Itero sobre cada campo, si esta en la lista, lo actualizo.
+    for campo in campos_actualizables:
+        if campo in data:
+            setattr(articulo, campo, data[campo])
+
     # este queda automatico, cada vez que se realice un Update, se actualiza la fecha automaticamente.
     articulo.actualizado = datetime.today()
     articulo.save()
